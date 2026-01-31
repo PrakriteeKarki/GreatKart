72f9207e0798adf6c5b600c15469db8c4987aaf1
PrakriteeKarki <prakritikarki940@gmail.com>
2026-01-31 07:51:57 +0545

    Update models.py

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -49,7 +49,7 @@ class Account(AbstractBaseUser,PermissionsMixin):
 
     #required
     date_joined=models.DateTimeField(auto_now_add=True)
-    last_login=models.DateTimeField(auto_now_add=True)
+    last_login=models.DateTimeField(auto_now=True)
     is_active=models.BooleanField(default=False)
     is_admin=models.BooleanField(default=False)
     is_superadmin=models.BooleanField(default=False)
